8f1e88f45642a7357f8ee7dd42266fd83f353afe
YoungForest <yangsen758@foxmail.com>
2017-12-05 00:06:43 +0800

    根据更新的文档，补充人员系统

--- assets/Script/firstscene/Person.js
+++ assets/Script/firstscene/Person.js
@@ -1,103 +1,179 @@
-var eState=cc.Enum({
-    working : 0,
-    free : 1,
-    relaxing : 2
-
+var eState = cc.Enum({
+    working: 0,
+    free: 1,
+    relaxing: 2
+});
+var Character = cc.Enum({
+    // B
+    imageKing: 0,
+    blabla: 1,
+    slience: 2,
+    funny: 3,
+    calm: 4,
+    optimist: 5,
+    serious: 6,
+    ambition: 7,
+    unyieelding: 8,
+    thoughtful: 9,
+    keen: 10,
+    persistent: 11,
+    // A
+    steady: 100,
+    bigOptimist: 101,
+    hardWork: 102,
+    bigAmnition: 103,
+    adversity: 104,
+    adjuvant: 105,
+    ghost: 106,
+    genius: 107,
+    // S
+    insight: 200,
+    storming: 201,
+    spirituality: 202
+});
+var Image = cc.Enum({
+    image0: 0,
+    image1: 1
+});
+var Sticker = cc.Enum({
+    sticker0: 0,
+    sticker1: 1
+});
+var Saying = cc.Enum({
+    ellisis: 0,
+    saying1: 1
+});
+var Occupation = cc.Enum({
+    develper: 0,
+    maintainer: 1,
+    marketer: 2
+});
+var Activity = cc.Enum({
+    activity0: 0,
+    activity1: 1
 });
 cc.Class({
     extends: cc.Component,
 
     properties: {
         /**能力值 */
-        coding_:0.,
-        science_:0.,
-        art_:0.,
-        creativity_:0.,
-        manager_:0.,
-        business_:0.,
+        gift_: 0.0,
+        coding_: 0.,
+        science_: 0.,
+        art_: 0.,
+        creativity_: 0.,
+        manager_: 0.,
+        business_: 0.,
         /*状态，用枚举表示，定义在上面 */
-        state_:{
-            default:eState.free,
-            type:eState,
+        state_: {
+            default: eState.free,
+            type: eState,
         },
         /**工资 */
-        salary_:0.,
-        employMoney_:0.,
-        name_:"",
-        profession_:"",
-        index_:0,
+        salary_: 0.,
+        employMoney_: 0.,
+        name_: "",
+        profession_: Occupation.develper,
+        index_: 0,
+        power: 10,
+        modd: 10,
     },
 
     // use this for initialization
     onLoad: function () {
-        
+
     },
-    init:function(person){
+    init: function (person) {
         //初始化人物属性
 
     },
-    develop:function(mamager,n,project){
-        var F = (1 / (n ^ (50 / (manager + 50)))) * (this.coding_ / 10) * (rand(0.9 , 1.1));    //功能
-        var P = (1 / (n ^ (50 / (manager + 50)))) * (this.science_ / 10) * (rand(0.9 , 1.1));   //性能
-        var E = (1 / (n ^ (50 / (manager + 50)))) * (this.art_ / 10) * (rand(0.9 , 1.1));       //体验
-        var I = (1 / (n^ (50 / (manager + 50)))) * (this.creativity_ / 10) * (rand(0.9 , 1.1)); //创意
-        var criticalPro = this.creativity_/(this.creativity_+200);
-        var criticalRate = 1+(5*sqrt(this.science_))/100;
-        if((rand(0.0 , 1.0)<criticalPro)){
-            F=F*criticalRate;
-            P=P*criticalRate;
-            E=E*criticalRate;
-            I=I*criticalRate;
+    develop: function (mamager, n, project) {
+        var F = (1 / (n ^ (50 / (manager + 50)))) * (this.coding_ / 10) * (rand(0.9, 1.1));    //功能
+        var P = (1 / (n ^ (50 / (manager + 50)))) * (this.science_ / 10) * (rand(0.9, 1.1));   //性能
+        var E = (1 / (n ^ (50 / (manager + 50)))) * (this.art_ / 10) * (rand(0.9, 1.1));       //体验
+        var I = (1 / (n ^ (50 / (manager + 50)))) * (this.creativity_ / 10) * (rand(0.9, 1.1)); //创意
+        var criticalPro = this.creativity_ / (this.creativity_ + 200);
+        var criticalRate = 1 + (5 * sqrt(this.science_)) / 100;
+        if ((rand(0.0, 1.0) < criticalPro)) {
+            F = F * criticalRate;
+            P = P * criticalRate;
+            E = E * criticalRate;
+            I = I * criticalRate;
         }
-        project.augment(0,F);
-        project.augment(1,P);
-        project.augment(2,E);
-        project.augment(3,I);
+        project.augment(0, F);
+        project.augment(1, P);
+        project.augment(2, E);
+        project.augment(3, I);
     },
-    begin:function(){
+    begin: function () {
         /**开始工作 */
-        this.state_=eState.working;
+        this.state_ = eState.working;
         //("开始工作");
     },
-    stop: function(){
+    stop: function () {
         /**停止工作 */
-        this.state_=eState.free;
+        this.state_ = eState.free;
         //cc.log("停止工作");
     },
-    setSalary:function(salary){
-        this.salary_=salary;
+    setSalary: function (salary) {
+        this.salary_ = salary;
     },
-    getSalary:function(){
+    getSalary: function () {
         return this.salary_;
     },
-    getEmployMoney:function(){
+    getEmployMoney: function () {
         return this.employMoney_;
     },
-    setEmployMoney:function(money){
-        this.employMoney_=money;
+    setEmployMoney: function (money) {
+        this.employMoney_ = money;
     },
-    setAbility:function(ability){
-        
+    setAbility: function (ability) {
+        this.gift_ = ability.gift_
+        this.coding_ = ability.coding_
+        this.science_ = ability.science_
+        this.art_ = ability.art_
+        this.creativity_ = ability.creativity_
+        this.manager_ = ability.manager_
+        this.business_ = ability.business_
     },
-    getAbility:function(){
-        
+    getAbility: function () {
+        var r = {
+            gift_ = this.gift_,
+            coding_ = this.coding_,
+            science_ = this.science_,
+            art_ = this.art_,
+            creativity_ = this.creativity_,
+            manager_ = this.manager_,
+            business_ = this.business_
+        }
+
+        return r
     },
-    setName:function(name){
-        this.name_=name;
+    setName: function (name) {
+        this.name_ = name;
     },
-    getName:function(){
+    getName: function () {
         return this.name_;
     },
-    setProfession:function(profession){
-        this.profession_=profession;
+    setProfession: function (profession) {
+        this.profession_ = profession;
     },
-    getProfession:function(){
+    getProfession: function () {
         return this.profession_;
     },
-    init:function(person){
+    init: function (person) {
 
     },
-    update:function(dt){
-        
+    update: function (dt) {
+
     },
+    sendSticker(sticker) {
+        cc.log(sticker)
+    },
+    speakSomething(saying) {
+        cc.log(saying)
+    },
+    moodIncrement(value) {
+        this.mood = (this.mood + value) % 10
+    }
 });
