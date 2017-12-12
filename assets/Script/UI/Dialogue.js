35eff62af4fb90a38615ebc25574703d95a6a048
YoungForest <yangsen758@foxmail.com>
2017-12-12 14:33:11 +0800

    Add init and new row logic.

--- assets/Script/UI/Dialogue.js
+++ assets/Script/UI/Dialogue.js
@@ -16,12 +16,30 @@ cc.Class({
             default: [],
             type: [cc.Label]
         },
+        head: 0,
+        tail: 0,
+        length_ : 10,
     },
 
     // use this for initialization
     onLoad: function () {
-
+        // 初始化length个对话气泡
+        for (var i = 0; i < length_; i++) {
+            var l = new cc.Label()
+            l.string = "test"
+            bubbles.push(l)
+        }
     },
+
+    newRow: function(s) {
+        if (tail >= length_) {
+            for (var i = this.head; i < tail-1; i++) {
+                bubbles[i].string = bubbles[i+1].string
+            }
+        }
+        bubbles[this.tail].string = s
+        this.tail ++
+    }
 
     // called every frame, uncomment this function to activate update callback
     // update: function (dt) {
